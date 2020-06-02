59e62917104071095ed2633d4c8a5df7c5b93ea0
alessandro <alessandrodigima@gmail.com>
2020-06-02 15:56:01 -0300

    incluido o arquivo .git ignore para ignorar os arquivos compilados - incluido template para inserção de dados na agenda via 'submit - POST'

--- core/views.py
+++ core/views.py
@@ -35,8 +35,24 @@ def submit_login(request):
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario) #aqui filtra pelo usuário autenticado
-                                     #   evento = Evento.objects.get(id=1)    --- objcts.get tras um registro
-
-    evento = Evento.objects.all()    #--- objcts.get tras um registro
+#   evento = Evento.objects.get(id=1)    --- objcts.get tras um registro
+#   evento = Evento.objects.all()    #--- objcts.get tras todos os registros
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
