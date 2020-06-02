49895bf43994a356b19a2661f91ee9f38574e599
alessandro <alessandrodigima@gmail.com>
2020-06-01 21:28:32 -0300

    renderização de páginas, e listagem de eventos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
 
 # Create your views here.
+
+
+#def index(request):
+#    return redirect('/agenda')
+
+
+def lista_eventos(request):
+    usuario = request.user
+ #   evento = Evento.objects.filter(usuario=usuario) #aqui filtra pelo usuário autenticado
+ #   evento = Evento.objects.get(id=1)    --- objcts.get tras um registro
+    evento = Evento.objects.all()    #--- objcts.get tras um registro
+
+    dados = {'eventos':evento}
+    return render(request, 'agenda.html', dados)
