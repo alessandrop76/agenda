cbbaa1375501e7916aa6852cc62abcb7e48aa008
alessandro <alessandrodigima@gmail.com>
2020-06-02 09:50:07 -0300

    configurando usuário e filtros de consulta por usuário, login e logout

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,11 +11,32 @@ from core.models import Evento
 #    return redirect('/agenda')
 
 
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválidos")
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
- #   evento = Evento.objects.filter(usuario=usuario) #aqui filtra pelo usuário autenticado
- #   evento = Evento.objects.get(id=1)    --- objcts.get tras um registro
-    evento = Evento.objects.all()    #--- objcts.get tras um registro
+    evento = Evento.objects.filter(usuario=usuario) #aqui filtra pelo usuário autenticado
+                                     #   evento = Evento.objects.get(id=1)    --- objcts.get tras um registro
 
+    evento = Evento.objects.all()    #--- objcts.get tras um registro
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
